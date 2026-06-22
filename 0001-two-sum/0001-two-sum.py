4ebf93d3efd857ad6db2796e5c8067a7baf3d6f0
Pavan Mhaisne <pavanmhaisne@gmail.com>
2026-06-22 23:58:38 +0530

    Time: 0 ms (100%), Space: 20.4 MB (41.51%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,16 @@
-# Sorting Method
+# Two Pass HashMap Method
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        A = []
-        for i, num in enumerate(nums):
-            A.append([num,i])
+        indices = {}
 
-        A.sort()
-        i, j = 0, len(nums)-1
-        while i < j:
-            cur = A[i][0] + A[j][0]
-            if cur == target:
-                return [ min(A[i][1],A[j][1]),
-                         max(A[i][1],A[j][1])]
-            elif cur < target:
-                i += 1
-            else:
-                j-= 1
+        for i , num in enumerate(nums):
+            indices[num] = i
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in indices and indices[diff] != i:
+                return [i,indices[diff]]
+
         return []
 
 # Synced seamlessly with LeetHub Pro
