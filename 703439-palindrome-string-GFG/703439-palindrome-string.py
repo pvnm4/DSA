865499248afa671083e98d00b29d36782b4baaf3
Pavan Mhaisne <pavanmhaisne@gmail.com>
2026-07-04 23:50:23 +0530

    Added solution - LeetHub

---
+++ 703439-palindrome-string-GFG/703439-palindrome-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s):
+        # code here
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+        
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
