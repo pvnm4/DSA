782c5c0e04b6a8c77dd695017234c6b06770d5ee
Pavan Mhaisne <pavanmhaisne@gmail.com>
2026-07-09 00:02:56 +0530

    Added solution - LeetHub

---
+++ 878829-min-cost-to-make-size-1-GFG/878829-min-cost-to-make-size-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def cost(self, arr):
+        #code here
+        n = len(arr)
+        return (n-1) * min(arr)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
